77b6b722bb39fa98464e6516994a988abde2356c
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2024-12-08 02:09:23 +0200

    alte chestii

--- script_two.js
+++ script_two.js
@@ -1,47 +1,59 @@
-let numere_runde_total = 5;
-let runde = 1;
 let humanScore = 0;
 let computerScore = 0;
 let human_selection;
-
+let selectii = ["btn_paper", "btn_scissors", "btn_rock"];
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+    let random = Math.floor(Math.random() * 3);
+    if(random == 0)
+        return selectii[0]
+    if(random == 1)
+        return selectii[1];
+    if(random ==2)
+        return selectii[2];
 }
 function playRound(getHumanChoice, getComputerChoice){
-    if(getComputerChoice == 0 && getHumanChoice == 1)
+    
+    if(getComputerChoice == "btn_paper" && getHumanChoice == "btn_scissors"){
+        list_element.textContent = "salut!";
         return humanScore++;
-    if(getComputerChoice == 1 && getHumanChoice == 2)
+    }
+    if(getComputerChoice == "btn_scissors" && getHumanChoice == "btn_rock"){ 
+        list_element.textContent = "salut!";
         return humanScore++;
-    if(getComputerChoice == 2 && getHumanChoice == 0)
+    }
+    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_paper")
         return humanScore++;
     /////
-    if(getComputerChoice == 1 && getHumanChoice == 0)
+    if(getComputerChoice == "btn_scissors" && getHumanChoice == "btn_paper")
         return computerScore++;
-    if(getComputerChoice == 2 && getHumanChoice == 1)
+    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_scissors")
         return computerScore++;
-    if(getComputerChoice == 0 && getHumanChoice == 2)
+    if(getComputerChoice == "btn_paper" && getHumanChoice == "btn_rock")
         return computerScore++;
     for(let i= 0; i<=2; i++)
-        if(getComputerChoice == i && getHumanChoice == i)
+        if(getComputerChoice == selectii[i] && getHumanChoice == selectii[i])
             return console.log("remiza");
+
 }
 
 const buttons = document.querySelectorAll("button");
 const round_numbers = document.querySelector("#round-numbers")
 const scor_om_id= document.querySelector("#scor-om-id")
 const scor_pc_id = document.querySelector("#scor-pc-id")
+const list = document.querySelector("ul");
 
 const runde_afisare = document.createElement("span"); //creăm un obiect pentru round_numbers sa afisam numerele
 round_numbers.appendChild(runde_afisare);
-round_numbers.textContent = runde + "/" + numere_runde_total;   
+
 
 const scor_om_afisare = document.createElement("span"); //creăm un obiect pentru round_numbers sa afisam numerele
 scor_om_id.appendChild(scor_om_afisare);
-scor_om_afisare.textContent = "Scor om:" + humanScore;
 
 const scor_pc_afisare = document.createElement("span"); //creăm un obiect pentru round_numbers sa afisam numerele
 scor_pc_id.appendChild(scor_pc_afisare);
-scor_pc_afisare.textContent = "Scor pc:" + computerScore;
+
+const list_element = document.createElement("li");
+
 
     //Funcția care așteaptă apăsarea unui buton
     function asteaptaApasareaButonului() {
@@ -58,17 +70,36 @@ scor_pc_afisare.textContent = "Scor pc:" + computerScore;
             buttons.forEach(buton => buton.addEventListener('click', handler));
         });
     }
-
+    function update_UI(scor_om,scor_pc,runde_actual,runde_total){
+        round_numbers.textContent = runde_actual + "/" + runde_total;
+        scor_om_afisare.textContent = "Scor om:" + scor_om;
+        scor_pc_afisare.textContent = "Scor pc:" + scor_pc;   
+    }
     // Apelăm funcția care așteaptă
     async function start() {
         while(1){
             let runde =prompt("How many rounds would you like to play?");
-            for (let i = 0; i < runde; i++) {
-                const human_selection = await asteaptaApasareaButonului();
+            for (let i = 1; i <= runde; i++) {
+
+                let human_selection = await asteaptaApasareaButonului();
                 let computer_selection = getComputerChoice();
+
                 playRound(human_selection, computer_selection);
+
+                round_numbers.textContent = i + "/" + runde;
+                scor_om_afisare.textContent = "Scor om:" + humanScore;
+                scor_pc_afisare.textContent = "Scor pc:" + computerScore;
+                list.appendChild(list_element);
             }
+            runde = 0;
+            humanScore=0;
+            computerScore=0;
+            break;
         }
     }
 
-start();
+start();    
+
+// 0 - hartie   btn_paper
+// 1 - foarfeca btn_scissors
+// 2 - piatra - btn_rock
