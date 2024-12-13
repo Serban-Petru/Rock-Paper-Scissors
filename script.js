56566cdc6ab7fb216bbb55470fcb859f8a90d9ef
Serban-Petru <177335339+Serban-Petru@users.noreply.github.com>
2024-12-13 13:29:14 +0200

    done for now

--- script.js
+++ script.js
@@ -31,27 +31,27 @@
 
 // // 0 - hartie
 // // 1 - foarfeca
-// // 2 - piatra
+// 2 - piatra
 
-// while(1){
-//     let runde =prompt("How many rounds would you like to play?");
-//     for (let i = 0; i < runde; i++) {
-//         let human_selection = getHumanChoice();
-//         let computer_selection = getComputerChoice();
-//         playRound(humanScore, computer_selection);
-//         console.log("H:" +humanScore + ", C:" + computerScore);
-//     }
-//     if(humanScore > computerScore)
-//         console.log("Ai castigat!");
-//     else if(humanScore < computerScore)
-//         console.log("Ai pierdut!");
-//     else
-//         console.log("Remiza!");
-//     if(prompt("Continue with another round?") == 'yes')
-//         continue;
-//     else
-//         break;
-// }
+while(1){
+    let runde =prompt("How many rounds would you like to play?");
+    for (let i = 0; i < runde; i++) {
+        let human_selection = getHumanChoice();
+        let computer_selection = getComputerChoice();
+        playRound(humanScore, computer_selection);
+        console.log("H:" +humanScore + ", C:" + computerScore);
+    }
+    if(humanScore > computerScore)
+        console.log("Ai castigat!");
+    else if(humanScore < computerScore)
+        console.log("Ai pierdut!");
+    else
+        console.log("Remiza!");
+    if(prompt("Continue with another round?") == 'yes')
+        continue;
+    else
+        break;
+}
 
 
 
--- script_two.js
+++ script_two.js
@@ -14,33 +14,46 @@ function getComputerChoice(){
 function playRound(getHumanChoice, getComputerChoice){
     
     if(getComputerChoice == "btn_paper" && getHumanChoice == "btn_scissors"){
-        list_element.textContent = "salut!";
+        adaugare_log(getHumanChoice,getComputerChoice,1)
         return humanScore++;
     }
     if(getComputerChoice == "btn_scissors" && getHumanChoice == "btn_rock"){ 
-        list_element.textContent = "salut!";
+        adaugare_log(getHumanChoice,getComputerChoice,1)
         return humanScore++;
     }
-    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_paper")
+    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_paper"){
+        adaugare_log(getHumanChoice,getComputerChoice,1) 
         return humanScore++;
+    }
     /////
-    if(getComputerChoice == "btn_scissors" && getHumanChoice == "btn_paper")
+    if(getComputerChoice == "btn_scissors" && getHumanChoice == "btn_paper"){ 
+        adaugare_log(getHumanChoice,getComputerChoice,0)
         return computerScore++;
-    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_scissors")
+    }
+    if(getComputerChoice == "btn_rock" && getHumanChoice == "btn_scissors"){ 
+        adaugare_log(getHumanChoice,getComputerChoice,0)
         return computerScore++;
-    if(getComputerChoice == "btn_paper" && getHumanChoice == "btn_rock")
+    }
+    if(getComputerChoice == "btn_paper" && getHumanChoice == "btn_rock"){
+        adaugare_log(getHumanChoice,getComputerChoice,0)
         return computerScore++;
+    }
     for(let i= 0; i<=2; i++)
-        if(getComputerChoice == selectii[i] && getHumanChoice == selectii[i])
+        if(getComputerChoice == selectii[i] && getHumanChoice == selectii[i]){
+            adaugare_log(getHumanChoice,getComputerChoice,2)
             return console.log("remiza");
+        }
 
 }
 
+const container =  document.querySelector("container");
 const buttons = document.querySelectorAll("button");
 const round_numbers = document.querySelector("#round-numbers")
 const scor_om_id= document.querySelector("#scor-om-id")
 const scor_pc_id = document.querySelector("#scor-pc-id")
 const list = document.querySelector("ul");
+const input_box = document.querySelector("input");
+const body = document.querySelector("body");
 
 const runde_afisare = document.createElement("span"); //creăm un obiect pentru round_numbers sa afisam numerele
 round_numbers.appendChild(runde_afisare);
@@ -51,9 +64,6 @@ scor_om_id.appendChild(scor_om_afisare);
 
 const scor_pc_afisare = document.createElement("span"); //creăm un obiect pentru round_numbers sa afisam numerele
 scor_pc_id.appendChild(scor_pc_afisare);
-
-const list_element = document.createElement("li");
-
 
     //Funcția care așteaptă apăsarea unui buton
     function asteaptaApasareaButonului() {
@@ -70,13 +80,51 @@ const list_element = document.createElement("li");
             buttons.forEach(buton => buton.addEventListener('click', handler));
         });
     }
+
     function update_UI(scor_om,scor_pc,runde_actual,runde_total){
         round_numbers.textContent = runde_actual + "/" + runde_total;
         scor_om_afisare.textContent = "Scor om:" + scor_om;
         scor_pc_afisare.textContent = "Scor pc:" + scor_pc;   
     }
+ 
+    function adaugare_log(human,pc,castig){
+        const list_element = document.createElement("li");
+        if(castig == 1) list_element.style.background = '#4ade45';
+        if(castig == 2) list_element.style.background = '#c850e6';
+        list_element.textContent = "Ai ales " + human + " pe cand PC " + pc;
+        list.appendChild(list_element);
+        list_element.scrollIntoView({ behavior: 'smooth' });
+    }
+    function alegere_castig(){
+        if(computerScore > humanScore){
+            const list_element = document.createElement("li")
+            list_element.style.background = '#b00201';
+            body.style.background = '#b00201';
+            list_element.textContent = "PC a castigat cu " + (computerScore-humanScore) + " diferenta.";
+            list.appendChild(list_element);
+            list_element.scrollIntoView({ behavior: 'smooth' });
+        }
+        if(computerScore < humanScore){
+            const list_element = document.createElement("li");
+            list_element.style.background = '#00b501';
+            body.style.background = '#00b501';
+            list_element.textContent = "Tu ai castigat cu " + (humanScore-computerScore) + " diferenta.";
+            list.appendChild(list_element);
+            list_element.scrollIntoView({ behavior: 'smooth' });
+        }
+        if(computerScore == humanScore){
+            const list_element = document.createElement("li");
+            list_element.style.background = '#7a837e';
+            body.style.background = '#7a837e';
+            list_element.textContent = "Remiza.";
+            list.appendChild(list_element);
+            list_element.scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+
     // Apelăm funcția care așteaptă
     async function start() {
+        //input_rutine();
         while(1){
             let runde =prompt("How many rounds would you like to play?");
             for (let i = 1; i <= runde; i++) {
@@ -89,15 +137,17 @@ const list_element = document.createElement("li");
                 round_numbers.textContent = i + "/" + runde;
                 scor_om_afisare.textContent = "Scor om:" + humanScore;
                 scor_pc_afisare.textContent = "Scor pc:" + computerScore;
-                list.appendChild(list_element);
+                
             }
+            alegere_castig();
             runde = 0;
             humanScore=0;
             computerScore=0;
+            // element.removeProperty('container');
             break;
         }
     }
-
+    
 start();    
 
 // 0 - hartie   btn_paper
